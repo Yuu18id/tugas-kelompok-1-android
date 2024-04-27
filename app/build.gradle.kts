348fbb0b9dd48ae5342304587bd7f3f4b38b4fbc
“Yuu18id“ <211112007@students.mikroskil.ac.id>
2024-04-27 14:50:36 +0700

    add: pembagian

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
--- app/src/main/java/com/stechoq/tugaskelompok1/PembagianActivity.kt
+++ app/src/main/java/com/stechoq/tugaskelompok1/PembagianActivity.kt
@@ -2,10 +2,38 @@ package com.stechoq.tugaskelompok1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
 
 class PembagianActivity : AppCompatActivity() {
+
+    private lateinit var angka1: AppCompatEditText
+    private lateinit var angka2: AppCompatEditText
+    private lateinit var tombolBagi: Button
+    private lateinit var hasil: TextView
+
+    private fun initComponents() {
+        angka1 = findViewById(R.id.bagiEditTextNum1)
+        angka2 = findViewById(R.id.bagiEditTextNum2)
+        tombolBagi = findViewById(R.id.bagiButtonResult)
+        hasil = findViewById(R.id.bagiTextResult)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembagian)
+
+        initComponents()
+
+        tombolBagi.setOnClickListener {
+            if (angka2.text.toString()!="0") {
+                val pembagian = angka1.text.toString().toDouble() / angka2.text.toString().toDouble()
+                hasil.text = pembagian.toString()
+            } else {
+                hasil.text = "Tidak bisa dibagi 0"
+            }
+
+        }
     }
 }
